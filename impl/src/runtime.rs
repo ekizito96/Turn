a89879278f13a8855569fb30969bfb04188fbb16
Muyukani Ephraim Kizito <muyukaniephraim@yahoo.com>
2026-02-24 17:16:48 +0300

    fix(ci): suppress manual_is_multiple_of clippy lint, fix test_actor.tn path

--- impl/src/runtime.rs
+++ impl/src/runtime.rs
@@ -181,6 +181,7 @@ impl SemanticMemory {
             self.items[new_idx].neighbors.push(curr);
 
             // Reassign entry point if this is structurally central (mock optimization)
+            #[allow(clippy::manual_is_multiple_of)]
             if new_idx % 10 == 0 {
                 self.entry_point = Some(new_idx);
             }
